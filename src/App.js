27693252bc561e61144c5879152902bfecabf093
Carva70 <nachocargar@gmail.com>
2023-02-11 21:55:18 +0100

    fix: navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
 import Nav from './Nav'
 import Fragment from './Fragment'
 import Payback from './Payback'
@@ -26,24 +24,10 @@ const router = createBrowserRouter([
   },
 ])
 
-const pageUrl =
-  'https://api.coingecko.com/api/v3/simple/price?ids=gods-unchained&vs_currencies=USD&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false&precision=18'
-
 function App() {
-  const [price, setPrice] = useState(0)
-
-  useEffect(() => {
-    axios
-      .get(pageUrl)
-      .then((res) => setPrice(res.data['gods-unchained']['usd']))
-      .catch((error) => {
-        console.error('Algo ha petado fuertemente', error)
-      })
-  }, [])
-
   return (
     <div>
-      <Nav price={parseFloat(price.toFixed(6))} />
+      <Nav />
       <RouterProvider router={router} />
     </div>
   )
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,23 @@
-import PropTypes from 'prop-types'
 import Container from 'react-bootstrap/Container'
 import BNav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Nav({ price }) {
+const pageUrl =
+  'https://api.coingecko.com/api/v3/simple/price?ids=gods-unchained&vs_currencies=USD&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false&precision=18'
+
+export default function Nav() {
+  const [price, setPrice] = useState(0)
+
+  useEffect(() => {
+    axios
+      .get(pageUrl)
+      .then((res) => setPrice(res.data['gods-unchained']['usd']))
+      .catch((error) => {
+        console.error('Algo ha petado fuertemente', error)
+      })
+  }, [])
   return (
     <div>
       <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
@@ -18,7 +32,7 @@ export default function Nav({ price }) {
             </BNav>
             <BNav className='justify-content-end'>
               <BNav.Link className='justify-content-end' eventKey='disabled' disabled>
-                GODS: <b>${price} </b>
+                GODS: <b>${price.toFixed(6)} </b>
               </BNav.Link>
             </BNav>
           </Navbar.Collapse>
@@ -26,9 +40,4 @@ export default function Nav({ price }) {
       </Navbar>
     </div>
   )
-}
-
-Nav.propTypes = {
-  price: PropTypes.number.isRequired,
-  onClickNavItem: PropTypes.func,
 }
